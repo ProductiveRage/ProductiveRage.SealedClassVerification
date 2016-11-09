2aeec9b52fd51a2e7bcd2a7a6c90d03aee9e7330
ProductiveRage <dangger36@gmail.com>
2016-11-09 18:48:35 +0000

    Added a .NET 4.5 build of the project, with its own NuGet packages
    The new project is .NET Core (builds in .NET Standard 1.6 and .NET 4.5
    but analysers don't apparently work well when pulled into .NET Core
    projects from a NuGet package, so currently there NuGet packages are for
    Bridge.NET projects and .NET 4.5+ projects only - see
    https://github.com/dotnet/roslyn-project-system/issues/285). Moved the
    attribute class file into the .NET Core project since .NET Core projects
    don't have "Add as link" support for existing files, while regular .NET
    class libraries do a good job of it.

--- BuildNuGetPackage/BuildFromRelease.bat
+++ BuildNuGetPackage/BuildFromRelease.bat
@@ -9,11 +9,18 @@ del *.pdb 2> nul
 del *.xml 2> nul
 del *.ps1 2> nul
 
-copy ..\"ProductiveRage.SealedClassVerification (Bridge)"\bin\Release\ProductiveRage.SealedClassVerification.dll > nul
-copy ..\"ProductiveRage.SealedClassVerification (Bridge)"\bin\Release\ProductiveRage.SealedClassVerification.xml > nul
-
 copy ..\Analyser\bin\Release\*.dll > nul
 copy ..\Analyser\bin\Release\tools\* > nul
 
+copy ..\"ProductiveRage.SealedClassVerification (Bridge)"\bin\Release\ProductiveRage.SealedClassVerification.dll > nul
+copy ..\"ProductiveRage.SealedClassVerification (Bridge)"\bin\Release\ProductiveRage.SealedClassVerification.xml > nul
+
 copy ..\ProductiveRage.SealedClassVerification.Bridge.nuspec > nul
 ..\packages\NuGet.CommandLine.2.8.5\tools\nuget pack -NoPackageAnalysis ProductiveRage.SealedClassVerification.Bridge.nuspec
+
+copy ..\"ProductiveRage.SealedClassVerification (.NET)"\bin\Release\net452\ProductiveRage.SealedClassVerification.dll > nul
+copy ..\"ProductiveRage.SealedClassVerification (.NET)"\bin\Release\net452\ProductiveRage.SealedClassVerification.xml > nul
+
+copy ..\ProductiveRage.SealedClassVerification.NET.nuspec > nul
+..\packages\NuGet.CommandLine.2.8.5\tools\nuget pack -NoPackageAnalysis ProductiveRage.SealedClassVerification.NET.nuspec
+
