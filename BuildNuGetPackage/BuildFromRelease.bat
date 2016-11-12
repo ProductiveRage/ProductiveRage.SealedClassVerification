716b538523efcd694ce06d3e4bc3163a53aefd16
ProductiveRage <dangger36@gmail.com>
2016-11-12 16:18:20 +0000

    Add support "netstandard1.6" to the NuGet package
    I didn't think that analysers worked correctly when included in NuGet
    packages consumed by .NET Core projects but it seems to work ok (the
    only problem I've found is that, while warnings are displayed in the
    Visual Studio Error List, there are no squigglies underlining the
    offending code)

--- BuildNuGetPackage/BuildFromRelease.bat
+++ BuildNuGetPackage/BuildFromRelease.bat
@@ -8,6 +8,7 @@ del *.dll 2> nul
 del *.pdb 2> nul
 del *.xml 2> nul
 del *.ps1 2> nul
+rd /s/q netstandard1.6 2> null
 
 copy ..\Analyser\bin\Release\*.dll > nul
 copy ..\Analyser\bin\Release\tools\* > nul
@@ -19,6 +20,8 @@ copy ..\ProductiveRage.SealedClassVerification.Bridge.nuspec > nul
 ..\packages\NuGet.CommandLine.2.8.5\tools\nuget pack -NoPackageAnalysis ProductiveRage.SealedClassVerification.Bridge.nuspec
 
 copy ..\"ProductiveRage.SealedClassVerification (.NET)"\bin\Release\net45\ProductiveRage.SealedClassVerification.Net.dll > nul
+md netstandard1.6
+copy ..\"ProductiveRage.SealedClassVerification (.NET)"\bin\Release\netstandard1.6\ProductiveRage.SealedClassVerification.Net.dll netstandard1.6 > nul
 
 copy ..\ProductiveRage.SealedClassVerification.NET.nuspec > nul
 ..\packages\NuGet.CommandLine.2.8.5\tools\nuget pack -NoPackageAnalysis ProductiveRage.SealedClassVerification.NET.nuspec
